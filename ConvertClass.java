cfcc3d2a8181bc6d50b20e50f7cdfba14be59ed6
poojakudale <pansarepoojab@gmail.com>
2020-12-05 12:53:26 +0530

    conversion demo class

---
+++ ConvertClass.java
@@ -0,0 +1,18 @@
+package com.simplilearn.test;
+
+public class ConvertClass {
+
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+			String val = "100";
+			double d = 10;
+			
+			//explicit conversion
+			int num =Integer.parseInt(val);
+			
+			System.out.println("num : "+num+"\nd : "+d);
+	}
+
+}
